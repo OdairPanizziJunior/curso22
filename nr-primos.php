f92142efbbf75074a39d2b7bac009d65e9ae2521
OdairPanizziJunior <odairjunior36@gmail.com>
2022-05-11 14:51:48 -0300

    numeros primos

---
+++ nr-primos.php
@@ -0,0 +1,38 @@
+<?php
+
+//Code By Ariel Filippi
+
+$primos = [2];
+$contPrimos = 0;
+
+$numero = 2;
+$divisor = 2;
+
+while ($contPrimos < 5) {
+
+    $contDiv = 0;
+    $numero++;
+
+    for ($divisor = 2; $divisor <= $numero; $divisor++) {
+        $resto = $numero % $divisor;
+
+        if ($resto == 0 && $divisor <= $numero) {
+            $contDiv++;
+        }
+
+        if ($contDiv >= 2) {
+            break;
+        }
+    }
+
+    if ($contDiv != 1) {
+        continue;
+    }
+
+    array_push($primos, $numero);
+    $contPrimos++;
+
+}
+
+$primosSTR = implode(", ", $primos);
+echo "Os 5 primos sao: " . $primosSTR;
