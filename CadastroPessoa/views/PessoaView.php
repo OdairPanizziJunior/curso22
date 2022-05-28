aac243f52d2b617eb9c5d6452586495eb615836c
OdairPanizziJunior <odairjunior36@gmail.com>
2022-05-27 23:19:20 -0300

    Formulário

--- CadastroPessoa/views/PessoaView.php
+++ CadastroPessoa/views/PessoaView.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <title>Cadastro Pessoa</title>
 </head>
-<body>
-
-        <form>
-
+    <body>
+    <form>
+           
             <div class="row mb-3"> <!-- $id  -->
-                <label class="col-sm-2 col-form-label">ID</label>
+                <label align="center" class="col-sm-2 col-form-label">ID</label>
                     <div class="col-sm-1">
                         <input type="number" class="form-control" placeholder="select">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $status -->
-                <label class="col-sm-2 col-form-label">Status:</label>
+                <label align="center" class="col-sm-2 col-form-label">Status:</label>
                     <div class="col-sm-2">
                         <select class="form-select">
                             <option selected>Choose...</option>
@@ -31,42 +31,42 @@
             </div>
 
             <div class="row mb-3"> <!-- $name  -->
-                <label class="col-sm-2 col-form-label">Name:</label>
-                    <div class="col-sm-4">
+                <label align="center" class="col-sm-2 col-form-label">Name:</label>
+                    <div class="col-sm-3">
                         <input class="form-control" type="text">
                     </div>
             </div>
         
             <div class="row mb-3"><!-- $mail -->
-                <label class="col-sm-2 col-form-label">E-mail:</label>
-                    <div class="col-sm-4">
+                <label align="center" class="col-sm-2 col-form-label">E-mail:</label>
+                    <div class="col-sm-3">
                         <input class="form-control" type="email">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $phone -->
-                <label class="col-sm-2 col-form-label">Phone:</label>
+                <label align="center" class="col-sm-2 col-form-label">Phone:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $CEP -->
-                <label class="col-sm-2 col-form-label">CEP:</label>
+                <label align="center" class="col-sm-2 col-form-label">CEP:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $socialweb -->
-                <label class="col-sm-2 col-form-label">Social Web:</label>
-                <div class="col-sm-4">
+                <label align="center" class="col-sm-2 col-form-label">Social Web:</label>
+                <div class="col-sm-3">
                     <input class="form-control" type="url">
                 </div>
             </div>
 
             <div class="row mb-3"><!-- $foundation / $born  -->
-                <label class="col-sm-2 col-form-label">Born/Foundation:</label>
+                <label align="center" class="col-sm-2 col-form-label">Born/Foundation:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="date">
                     </div>
@@ -74,7 +74,7 @@
 
             <div>
                 <fieldset class="row mb-3"> <!-- $CPF / $CNPJ  -->
-                    <legend class="col-form-label col-sm-2 pt-0">Type</legend>
+                    <legend align="center" class="col-form-label col-sm-2 pt-0">Type</legend>
                         <div class="col-sm-10">
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="gridRadios1" value="CPF" checked>
@@ -91,9 +91,9 @@
                 </fieldset>
             </div>
 
-            <div class="row mb-3"><!-- $marital_status -->
-            <fieldset class="row mb-3"> 
-                    <legend class="col-form-label col-sm-2 pt-0">Marietal Status:</legend>
+            <div class="row mb-0"><!-- $marital_status -->
+                <fieldset class="row mb-3"> 
+                    <legend align="center" class="col-form-label col-sm-2 pt-0">Marietal Status:</legend>
                         <div class="col-sm-10">
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="gridRadios2" value="CPF" checked>
@@ -111,9 +111,9 @@
                 </fieldset>
             </div>
 
-            <div class="row mb-3"><!-- $gender -->
-            <fieldset class="row mb-3"> 
-                    <legend class="col-form-label col-sm-2 pt-0">Gender:</legend>
+            <div class="row mb-0"><!-- $gender -->
+                <fieldset class="row mb-3"> 
+                    <legend align="center" class="col-form-label col-sm-2 pt-0">Gender:</legend>
                         <div class="col-sm-10">
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="gridRadios3" value="CPF" checked>
@@ -132,35 +132,35 @@
             </div>
 
             <div class="row mb-3"><!-- $RG -->
-                <label class="col-sm-2 col-form-label">RG:</label>
+                <label align="center" class="col-sm-2 col-form-label">RG:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $PIS -->
-                <label class="col-sm-2 col-form-label">PIS:</label>
+                <label align="center" class="col-sm-2 col-form-label">PIS:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $profession -->
-                <label class="col-sm-2 col-form-label">Profession:</label>
+                <label align="center" class="col-sm-2 col-form-label">Profession:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div>
             </div>
 
             <div class="row mb-3"><!-- $CNAE -->
-                <label class="col-sm-2 col-form-label">CNAE:</label>
+                <label align="center" class="col-sm-2 col-form-label">CNAE:</label>
                     <div class="col-sm-2">
                         <input class="form-control" type="text">
                     </div> 
             </div>
             
             <div class="row mb-3"><!-- $schooling -->
-                <label class="col-sm-2 col-form-label">Schooling:</label>
+                <label align="center" class="col-sm-2 col-form-label">Schooling:</label>
                     <div class="col-sm-2">
                         <select class="form-select">
                             <option selected>Choose...</option>
@@ -172,10 +172,9 @@
                     </div>
             </div>
         
-            <button type="submit" class="btn btn-primary">Confirm</button>
-            
+            <!-- <button type="submit" class="btn btn-primary">Confirm</button> -->
+           
         </form>
-
-
-</body>
+        
+    </body>
 </html>
