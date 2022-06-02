18f0e53136401b12da28e151508124f3ed9f6aa1
OdairPanizziJunior <odairjunior36@gmail.com>
2022-06-02 19:29:50 -0300

    Cadastro

--- CadastroPessoa/views/js/pessoa.js
+++ CadastroPessoa/views/js/pessoa.js
@@ -2,12 +2,12 @@ $( document ).ready(function() {
 
     $(".btnEditar").on("click", function(){
         var id = $(this).val();
-        console.log("Sou o clicar do editar id:" + id);
+        console.log("I'm click of Edit button id: " + id);
     });
 
     $(".btnExcluir").on("click", function(){
         var id = $(this).val();
-        console.log("Sou o clicar do excluir" + id);
+        console.log("I'm click of Delete button id: " + id);
     });
 
 
