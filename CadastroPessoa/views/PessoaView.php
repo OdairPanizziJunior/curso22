6a26c5995dae51f88a8748b7415a1956b7961de8
OdairPanizziJunior <odairjunior36@gmail.com>
2022-06-07 21:55:46 -0300

    CadastroPessoa

--- CadastroPessoa/views/PessoaView.php
+++ CadastroPessoa/views/PessoaView.php
@@ -219,41 +219,8 @@
                                 <td>odair@curso.com</td>
                                 <td>(54) 91234-5678</td>
                                 <td>
-                                    <button class="btnEditar btn btn-primary" value="1">Edit</button>
-                                    <button class="btnExcluir btn btn-primary" value="1">Delete</button>
-                                </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                                <td>João Gordo</td>
-                                <td>CPF</td>
-                                <td>fatguy@mcdonalds.com</td>
-                                <td>(51) 99988-7766</td>
-                                <td>
-                                    <button class="btnEditar btn btn-primary" value="2">Edit</button>
-                                    <button class="btnExcluir btn btn-primary" value="2">Delete</button>
-                                </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                                <td>Larry the Bird</td>
-                                <td>CNPJ</td>
-                                <td>comercial@larry.com</td>
-                                <td>(88) 98745-6123</td>
-                                <td>
-                                    <button class="btnEditar btn btn-primary" value="3">Edit</button>
-                                    <button class="btnExcluir btn btn-primary" value="3">Delete</button>
-                                </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                                <td>Escola Infoserv</td>
-                                <td>CNPJ</td>
-                                <td>bento@escolainfoserv.com.br</td>
-                                <td>(54) 3452-6666</td>
-                                <td>
-                                    <button class="btnEditar btn btn-primary" value="4">Edit</button>
-                                    <button class="btnExcluir btn btn-primary" value="4">Delete</button>
+                                    <button class="btnEditar btn btn-primary" value="10">Edit</button>
+                                    <button class="btnExcluir btn btn-primary" value="5">Delete</button>
                                 </td>
                         </tr>
                     </tbody>
--- CadastroPessoa/views/js/pessoa.js
+++ CadastroPessoa/views/js/pessoa.js
@@ -1,13 +1,27 @@
 $( document ).ready(function() {
 
+    var baseUrl ="http://172.27.237.166/curso22/CadastroPessoa/controllers/PessoaController.php";
+
     $(".btnEditar").on("click", function(){
-        var id = $(this).val();
-        console.log("I'm click of Edit button id: " + id);
+        var idPessoa = $(this).val();
+        baseUrl += "?id=" + idPessoa;
+
+        $.get(baseUrl).done(function(response){
+            console.log(JSON.stringify(response));
+        });
+
+        console.log("I'm click of Edit button id: " + idPessoa);
     });
 
     $(".btnExcluir").on("click", function(){
-        var id = $(this).val();
-        console.log("I'm click of Delete button id: " + id);
+        var idPessoa = $(this).val();
+        
+
+        $.post(baseUrl, { id: idPessoa }).done(function(response){
+            console.log(JSON.stringify(response))
+        });
+
+        console.log("I'm click of Delete button id: " + idPessoa);
     });
 
 
