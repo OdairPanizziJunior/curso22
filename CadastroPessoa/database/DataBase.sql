da6f91baac75f07d2c7257126cc3f1e8e6ec75bd
OdairPanizziJunior <odairjunior36@gmail.com>
2022-07-07 19:48:59 +0100

    cadastro pessoas

---
+++ CadastroPessoa/database/DataBase.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: curso22
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.0.38-MariaDB-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pessoas`
+--
+
+DROP TABLE IF EXISTS `pessoas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoas` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(250) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `cnpj` varchar(14) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `genero` varchar(3) DEFAULT NULL,
+  `cep` varchar(8) DEFAULT NULL,
+  `telefone` varchar(11) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `userupdated` varchar(100) DEFAULT NULL,
+  `created` datetime DEFAULT null,
+  `usercreated` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoas`
+--
+
+LOCK TABLES `pessoas` WRITE;
+/*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
+INSERT INTO `pessoas` VALUES (1,'Ariel','ariel@info.com','12354678910','',0,'M','95700000','999524677',0,'2022-07-05 20:44:41','','2022-07-05 20:44:41','ariel_admin'),(2,'Rodrigo','rubbo@info.com','12354678910','',0,'M','95700000','999528527',0,NULL,'','2022-07-05 20:48:29','ariel_admin'),(3,'Ricier','ricier@info.com','12354678910','',0,'M','95700000','999524566',0,'2022-07-05 20:51:02','','2022-07-05 20:49:38','ariel_admin');
+/*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'curso22'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-05 21:33:59
